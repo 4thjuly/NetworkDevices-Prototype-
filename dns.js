6e3664d85904508641b4c8500d50bd0bddc6249c
4thJuly <4thJuly>
2013-09-05 13:27:14 -0700

    Starting mdns support

---
+++ dns.js
@@ -0,0 +1,25 @@
+// Handle all dns related network stuff
+
+
+var DNSQuery = {
+	transactionId = 0;
+	flags = 0;
+	questionResources = DNSQuestionEntry[];
+	answerResources = DNSResourceRecord[];
+	authorityResources = DNSResourceRecord[];
+	additionalResources = DNSResourceRecord[];
+}
+
+var DNSQuestionEntry = {
+	name = '';
+	type = 0;	
+}
+	
+var DNSResourceRecord = {
+	name = '';
+	type = 0;
+	class = 1;
+	ttl = 0;
+	data = new ArrayBuffer();
+}
+	
